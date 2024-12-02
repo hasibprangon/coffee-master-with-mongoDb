ee462257fb0e4c3766e3344ea8464a90b27b1ffb
hasibprangon <hasibulhossainp09@gmail.com>
2024-12-02 19:57:25 +0600

    perform delete operation for delete a coffee from db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -35,6 +35,14 @@ async function run() {
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result)
+    })
+
+
+    app.delete('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
     })
 
 
